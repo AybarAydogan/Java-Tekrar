843aabdc76414212673ae1f24ce68cb30913694c
Aybar <you@example.com>
2023-10-27 10:08:06 +0300

    day 8

---
+++ src/day7/P01.java
@@ -0,0 +1,22 @@
+package day7;
+
+public class P01 {
+
+    String isim="Aybar"; String soyisim="Aydoğan";
+
+    static int yas=19;
+
+    static double kutle=93.0;
+
+    public static void main(String[] args) {
+
+        System.out.println(kutle);
+        System.out.println(yas);
+
+    }
+    public void staticOlmayan(){
+        System.out.println(kutle);
+        System.out.println(yas);
+        System.out.println(isim);
+    }
+}
